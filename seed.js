6ddc15c330c84be794dd06c029243c28f3d5e90f
DanielRevenga <daniel.revenga.garcia@gmail.com>
2022-02-03 22:35:18 +0100

    images

--- seed.js
+++ seed.js
@@ -22,25 +22,7 @@ mongoose.connect(`mongodb+srv://snake:1234@cluster0.cfo5h.mongodb.net/Miranda?re
 	console.log("Mongoose Is Connected");
 };
 
-const bookings_seed = async () => {
-    console.log("BOOKINGS SEED START");
-    try {
-        // DELETE BOOKINGS
-        await Booking.deleteMany({});
-
-        // INSERT BOOKINGS
-        for(let el of bookings_data) {
-            let booking = new Booking(el);
-            await booking.save()
-                .then(response => {}) 
-                .catch(error => console.log("BOOKING"+error));
-        }
-    } catch (error) {
-        console.log({error: "BOOKING"+error});
-    }
-    console.log("BOOKINGS SEED START");
-}
-
+// ROOMS
 const rooms_seed = async () => {
     console.log("ROOMS SEED START");
     try {
@@ -58,26 +40,6 @@ const rooms_seed = async () => {
         console.log({error: "ROOM"+error});
     }
     console.log("ROOMS SEED START");
-}
-
-// INSERT CONTACTS
-const contacts_seed = async () => {
-    console.log("CONTACTS SEED START");
-    try {
-        // DELETE CONTACTS
-        await Contact.deleteMany({});
-
-        // INSERT CONTACTS
-        for(let el of contacts_data) {
-            let contacts = new Contact(el);
-            await contacts.save()
-                .then(response => {}) 
-                .catch(error => console.log("CONCTACT"+error));
-        }
-    } catch (error) {
-        console.log({error: "CONCTACT"+error});
-    }
-    console.log("CONTACTS SEED END");
 }
 
 // INSERT USERS
@@ -100,13 +62,75 @@ const users_seed = async () => {
     console.log("USERS SEED END");
 }
 
+// CONTACTS
+const contacts_seed = async () => {
+    console.log("CONTACTS SEED START");
+    try {
+        // DELETE CONTACTS
+        await Contact.deleteMany({});
+
+        // INSERT CONTACTS
+        for(let el of contacts_data) {
+            let contacts = new Contact(el);
+            await contacts.save()
+                .then(response => {}) 
+                .catch(error => console.log("CONCTACT"+error));
+        }
+    } catch (error) {
+        console.log({error: "CONCTACT"+error});
+    }
+    console.log("CONTACTS SEED END");
+}
+
+
+
+
+// BOOKINGS
+const bookings_seed = async () => {
+    console.log("BOOKINGS SEED START");
+    try {
+        // DELETE BOOKINGS
+        await Booking.deleteMany({});
+
+        // INSERT BOOKINGS
+        for(let el of bookings_data) {
+            
+            await Room.countDocuments({}, (err, numDocs) => {
+                let rand = Math.floor(Math.random() * numDocs);
+                Room.findOne().skip(rand).exec(async(err, result) => {
+                    if (result) el.room_id = result._id;
+
+                    await User.countDocuments({}, (err, numDocs) => {
+                        let rand = Math.floor(Math.random() * numDocs);
+                        User.findOne().skip(rand).exec(async(err, result2) => {
+                            
+                            if (result2) el.user_id = result2._id;
+                            const booking = new Booking(el);
+
+                            await booking.save()
+                                .then(response => {}) 
+                                .catch(error => console.log("BOOKING"+error));  
+                        });
+                    })
+                  
+                });               
+            })
+              
+            
+        }
+    } catch (error) {
+        console.log({error: "BOOKING"+error});
+    }
+    console.log("BOOKINGS SEED END");
+}
+
 const execute_seeds = async () => {
-    await bookings_seed();
     await rooms_seed();
     await contacts_seed();
     await users_seed();
+    await bookings_seed();
     
-    mongoose.connection.close();
+    // mongoose.connection.close();
 }
 
 execute_seeds();
